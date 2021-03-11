463887554ee74262fc5fef5157b66f93f2748f42
Dmazx <76870981+Dmazx@users.noreply.github.com>
2021-03-11 10:09:18 +0800

    Add files via upload
    just share what I know with with others, because I like to share knowledge with other people, and I'm learning english

---
+++ login_final.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // create variabel4
+    string password, username;
+    string username1 = "Dmazx10";
+    string pinpassword = "programmer1023";
+
+    // user input
+    cout << "===== login forum ===== \n\n";
+    cout << "username= ";
+    getline (cin, username);
+    cout << "password= ";
+    getline (cin, password);
+
+    // condicition and execution
+    if (username == username1) {
+        if (password == pinpassword) {
+            cout << "login success\nyoure welcome";
+        }
+    }
+    else {
+        cout << "try again";
+    }
+    
+    cout << endl;
+    return 0;
+}
